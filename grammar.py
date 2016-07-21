68bd37b3afc817853aad259f4e3063d7813d8bde
Robert Coleman <rcoleman@fool.com>
2016-07-20 20:39:06 -0400

    Update grammar.py
    improved sentence tokenizing

--- grammar.py
+++ grammar.py
@@ -2,26 +2,40 @@ from stat_parser import Parser
 from nltk.parse.generate import generate, _generate_all
 from nltk import Nonterminal, induce_pcfg
 from random import choice
+from nltk.tokenize import PunktSentenceTokenizer
+from contractions import contractions
 
 
+sent_tokenizer = PunktSentenceTokenizer()
 
 with open("<source of text>", "r") as f:
-            sents = f.read().replace(',', '').replace('(', '').replace('(', '').split("\n")
+    text = f.read()
+
+for k, v in contractions.items():
+    text = text.replace(k, v)
+
+sents = []
+for paragraph in text.split('\n'):
+    sents += sent_tokenizer.tokenize(paragraph)
 
 parser = Parser()
-productions = []
 
-for sent in sents[:-2]:
-	try:
-		tree = parser.parse(sent)
-		productions += tree.productions()
-	except:
-		pass
+productions = []
+for sent in sents[:10]:
+    try:
+        tree = parser.parse(sent)
+        productions += tree.productions()
+    except:
+        pass
 
 
 S = Nonterminal('S')
 grammar = induce_pcfg(S, productions)
 
 
-for sentence in generate(grammar, depth=5):
-	print sentence
+with open("C:/Projects/playground/bobot/bobot/sents.doc", "w") as f:
+    for sentence in generate(grammar, depth=5):
+        print " ".join(sentence) + "\n"
+        if max([sentence.count(x) for x in sentence]) <= 3:
+            f.write(" ".join(sentence) + ".\n")
+            print " ".join(sentence) + "\n"
